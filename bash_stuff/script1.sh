0ac818fb2765c30a0b9621424ceed94e57ccbf32
voxmenthe <jeffcogg@gmail.com>
2018-12-25 11:36:17 -0800

    bash stuff

---
+++ bash_stuff/script1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Setting vars
+var1=input.txt
+dir1=test.d
+
+# Executing commands
+echo "Var 1 is set to: $var1"
+cd $dir1
+pwd
---
+++ bash_stuff/script2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Setting vars
+var1="1.txt 2.txt 3.txt 4.txt"
+
+# For loop
+for i in $var1 ; do
+		echo $i
+done
